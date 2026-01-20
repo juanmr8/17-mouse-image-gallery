d1f42ae50b131277f77f513dc9163340999aa038
juanmr8 <moraromerojuan8@gmail.com>
2026-01-20 11:08:40 +0100

    modified the hook

--- hooks/use-mouse-position.tsx
+++ hooks/use-mouse-position.tsx
@@ -2,55 +2,55 @@ import * as React from "react";
 
 
 export default function useMouse() {
-  const [state, setState] = React.useState({
-    x: 0,
-    y: 0,
-    elementX: 0,
-    elementY: 0,
-    elementPositionX: 0,
-    elementPositionY: 0,
-  });
-  const ref = React.useRef<HTMLDivElement>(null);
+	const [state, setState] = React.useState({
+		x: 0,
+		y: 0,
+		elementX: 0,
+		elementY: 0,
+		elementPositionX: 0,
+		elementPositionY: 0,
+	});
+	const ref = React.useRef<HTMLDivElement>(null);
 
-  const handleMouseMove = (e: MouseEvent) => {
-    const { pageX, pageY } = e;
-    let newState = {
-      x: pageX,
-      y: pageY,
-      elementX: 0,
-      elementY: 0,
-      elementPositionX: 0,
-      elementPositionY: 0,
-    };
-    if (ref.current) {
-      const { left, top } = ref.current.getBoundingClientRect();
-      const elementPositionX = left + window.scrollX;
-      const elementPositionY = top + window.scrollY;
-      const elementX = pageX - elementPositionX;
-      const elementY = pageY - elementPositionY;
+	const handleMouseMove = (e: MouseEvent) => {
+		const { pageX, pageY } = e;
+		let newState = {
+			x: pageX,
+			y: pageY,
+			elementX: 0,
+			elementY: 0,
+			elementPositionX: 0,
+			elementPositionY: 0,
+		};
+		if (ref.current) {
+			const { left, top } = ref.current.getBoundingClientRect();
+			const elementPositionX = left + window.scrollX;
+			const elementPositionY = top + window.scrollY;
+			const elementX = pageX - elementPositionX;
+			const elementY = pageY - elementPositionY;
 
-      newState.elementX = elementX;
-      newState.elementY = elementY;
-      newState.elementPositionX = elementPositionX;
-      newState.elementPositionY	 = elementPositionY;
-    }
-    setState((s: typeof state) => {
-      return {
-        ...s,
-        ...newState,
-      };
-    });
-  };
+			newState.elementX = elementX;
+			newState.elementY = elementY;
+			newState.elementPositionX = elementPositionX;
+			newState.elementPositionY = elementPositionY;
+		}
+		setState((s: typeof state) => {
+			return {
+				...s,
+				...newState,
+			};
+		});
+	};
 
-  React.useLayoutEffect(() => {
-    document.addEventListener("mousemove", handleMouseMove);
+	React.useLayoutEffect(() => {
+		document.addEventListener("mousemove", handleMouseMove);
 
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-    };
-  }, []);
+		return () => {
+			document.removeEventListener("mousemove", handleMouseMove);
+		};
+	}, []);
 
 
 
-  return [state, ref] as const;
+	return [state, ref] as const;
 }
